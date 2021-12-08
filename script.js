0cf1f5b5b634bc8c373ea65daded1ee16c55c57c
finnius25 <finneykoshy@gmail.com>
2021-12-08 14:51:52 -0600

    create color btn

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
+const container = document.querySelector(".container");
+const blackBtn = document.createElement('button');
+const colorBtn = document.createElement('button');
+const eraserBtn = document.createElement('button');
+const sizeBtn = document.createElement('button');
+const btnContainer = document.querySelector(".buttons");
 
-function defaultView () {
-    const container = document.querySelector(".container");
-    for (i = 0; i < 16; i++) {
-        container.innerHTML += '<div class="box"></div>';
+function gridSize (col, rows) {
+    for (i = 0; i < (col * rows); i++) {
+        const div = document.createElement('div');
+        container.style.gridTemplateColumns = `repeat(${col}, ifr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, ifr)`;
+        container.appendChild(div).classList.add('box')
     };
 };
 
-defaultView();
 
-let changeColor = () => {
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach((box) => {
-        box.addEventListener('mouseover', () => {
-            // div.classList.add("color-change")
-            console.log('hello');
-        });
-    });
-};
+
+gridSize(4,4);
+
+function rdmColor() {
+    const boxes = document.querySelectorAll(".box");
+    colorBtn.textContent = 'Color';
+    colorBtn.addEventListener('click', () =>{
+        boxes.forEach(box => box.addEventListener('mouseover', () => {
+            const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+            const r = randomBetween(0, 255);
+            const g = randomBetween(0, 255);
+            const b = randomBetween(0, 255);
+            box.style.background = `rgb(${r},${g},${b})`
+        }))
+    })
+    btnContainer.appendChild(colorBtn).classList.add('btn');
+}
+rdmColor();
