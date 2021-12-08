7c14217d8a24e61bd1c266503e5f3af2be3a399e
finnius25 <finneykoshy@gmail.com>
2021-12-08 15:30:58 -0600

    create black btn, eraser btn, size btn

--- script.js
+++ script.js
@@ -8,15 +8,13 @@ const btnContainer = document.querySelector(".buttons");
 function gridSize (col, rows) {
     for (i = 0; i < (col * rows); i++) {
         const div = document.createElement('div');
-        container.style.gridTemplateColumns = `repeat(${col}, ifr)`;
-        container.style.gridTemplateRows = `repeat(${rows}, ifr)`;
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         container.appendChild(div).classList.add('box')
     };
 };
 
-
-
-gridSize(4,4);
+gridSize(16,16);
 
 function rdmColor() {
     const boxes = document.querySelectorAll(".box");
@@ -33,3 +31,55 @@ function rdmColor() {
     btnContainer.appendChild(colorBtn).classList.add('btn');
 }
 rdmColor();
+
+function blackColor() {
+    const boxes = document.querySelectorAll(".box");
+    blackBtn.textContent = 'black';
+    blackBtn.addEventListener('click', () =>{
+        boxes.forEach(box => box.addEventListener('mouseover', () => {
+            box.style.background = 'Black';
+        }))
+    })
+    btnContainer.appendChild(blackBtn).classList.add('btn');
+} 
+blackColor();
+
+function eraser() {
+    const boxes = document.querySelectorAll(".box");
+    eraserBtn.textContent = 'Eraser';
+    eraserBtn.addEventListener('click', () =>{
+        boxes.forEach(box => box.addEventListener('mouseover', () => {
+            box.style.background = 'white';
+        }))
+    })
+    btnContainer.appendChild(eraserBtn).classList.add('btn');
+} 
+eraser();
+
+function reSet() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.remove())
+}
+
+function reSize() {
+    sizeBtn.textContent = "size"
+    sizeBtn.addEventListener('click', () => {
+        let user = prompt('Enter Size');
+        if (user === null || user < 1) {
+            reSet();
+            gridSize(16,16);
+            rdmColor();
+            blackColor();
+            eraser();
+        } else {
+            reSet();
+            gridSize(user,user);
+            rdmColor();
+            blackColor();
+            eraser();
+        }
+    })
+    btnContainer.appendChild(sizeBtn).classList.add('btn');
+}
+reSize();
+
